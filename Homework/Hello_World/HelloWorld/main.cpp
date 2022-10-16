41236101eaf1063a750a477893180d34e310532e
Sean Gillam <sean.gillam.git@gmail.com>
2022-10-15 21:35:28 -0700

    Signed-off-by: Sean Gillam <sean.gillam.git@gmail.com>

--- Homework/Hello_World/HelloWorld/main.cpp
+++ Homework/Hello_World/HelloWorld/main.cpp
@@ -1,22 +1,21 @@
-//System Libraries Here
+// Get firstname/lastname
 #include <iostream>
-using namespace std;
-
-enum test
-{
-    zero,
-    one,
-    two,
-    three
-};
-
-int main(int argc, char** argv) 
+#include <string>
+	
+int main()	
 {
     int i;
-    for(auto test : {zero, one, two, three})
-    {
-        i = test;
-        cout<<i;
-    }
+
+    std::string firstname;
+    std::string lastname;
+    std::cout<<"Enter First Name? ";
+    std::cin>>firstname;
+    std::cin.ignore();
+    for(i=0; firstname[i] ; i++) firstname [i]=toupper(firstname[i]);
+    std::cout<<"Enter Last Name? ";
+    std::cin>>lastname;
+    std::cin.ignore();
+    for(i=0; lastname[i] ; i++) lastname [i]=toupper(lastname[i]);	
+    std::cout << firstname << " " << lastname;
     return 0;
 }
